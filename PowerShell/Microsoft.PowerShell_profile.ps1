bbc816de3090615b5b8676b48b24425c87dca94b
Tsubura Kono <51806338+tkonoansys@users.noreply.github.com>
2025-03-24 12:30:46 +0900

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,7 @@
-
-#
-## PowerShell modules ##
-#
-# Install-Module -Name posh-git -Scope CurrentUser -Force
-# https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
-
-[string]$CWD = $PWD.Path
-
+# Configure PSReadLine.
 if (Get-InstalledPSResource -Name 'PSReadLine') {
     Import-Module PSReadLine
+
     $PSROptions = @{
         EditMode = "Emacs"
         PredictionSource = "HistoryAndPlugin"
@@ -21,20 +14,20 @@ if (Get-InstalledPSResource -Name 'PSReadLine') {
     Set-PSReadLineOption @PSROptions
 }
 
+# Install-Module -Name posh-git -Scope CurrentUser -Force
+# https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
 if (Get-InstalledPSResource -Name 'posh-git') {
     Import-Module posh-git
 
-    [string]$RgbPromptColor1 = '0x00BFFF' # DeepSkyBlue
-    [string]$RgbPromptColor2 = '0x40E0D0' # Turquoise
-
     $GitPromptSettings.DefaultPromptPrefix.Text = "PS "
-    $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = $RgbPromptColor1
-    $GitPromptSettings.DefaultPromptPath.ForegroundColor = $RgbPromptColor2
-    $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = $RgbPromptColor1
-    $GitPromptSettings.DefaultPromptSuffix.Text = "$('>' * ($nestedPromptLevel + 1)) "
+    $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = '0x00BFFF' # DeepSkyBlue
+    $GitPromptSettings.DefaultPromptPath.ForegroundColor = '0x40E0D0' # Turquoise
+    $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = '0x00BFFF' # DeepSkyBlue
 } else {
-    [string]$ESC27 = "$([char]27)"
-    $out = "${ESC27}[38;5;45mPS ${ESC27}[0m${ESC27}[38;5;140m${CWD}$('>' * ($nestedPromptLevel + 1))${ESC27}[0m "
+    [string]$StartOfColor = "$([char]27)[38;5;"
+    [string]$EndOfColor = "$([char]27)[0m"
+
+    $out = "${StartOfColor}45mPS ${EndOfColor}${StartOfColor}51m${PWD}>${$EndOfColor} "
 }
 
 if ($env:TERM_PROGRAM -ne 'vscode') {
@@ -45,8 +38,6 @@ if ($env:TERM_PROGRAM -ne 'vscode') {
         [string]$OSC133 = "`e]133"
 
         $FakeCode = [int]!$global:?
-        # NOTE: We disable strict mode for the scope of this function because it unhelpfully throws an
-        # error when $LastHistoryEntry is null, and is not otherwise useful.
         Set-StrictMode -Off
         $LastHistoryEntry = Get-History -Count 1
         $Result = ""
@@ -57,7 +48,6 @@ if ($env:TERM_PROGRAM -ne 'vscode') {
                 $Result += "${OSC133};D`a"
             } else {
                 # Command finished exit code
-                # OSC 133 ; D [; <ExitCode>] ST
                 $Result += "${OSC133};D;${FakeCode}`a"
             }
         }
@@ -65,7 +55,7 @@ if ($env:TERM_PROGRAM -ne 'vscode') {
         # Prompt started
         $Result += "${OSC133};A${ESC07}"
         # Current Working Directory
-        $Result += "`e]9;9;`"${CWD}`"${ESC07}"
+        $Result += "`e]9;9;`"${PWD}`"${ESC07}"
 
         if (Get-Module -Name 'posh-git') {
             $Global:__OriginalPrompt = ${GitPromptScriptBlock}
